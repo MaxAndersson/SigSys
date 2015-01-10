d3d48674f6af1ea85411335d51bb0c1e54fed0b9
Max Andersson <MaxN90@gmail.com>
2015-01-10 19:00:06 +0100

    lol

--- SigSys/SigSys/src/main.c
+++ SigSys/SigSys/src/main.c
@@ -1,17 +1,26 @@
+
 /*********************************************************************************
- * This project is designed for the course Embedded Signal Processing Systems
- * The program provides a software framework that has implemented 2xADC, 2xDAC,
- * an external trigger and an USART. Therefore, one can
- *   - access the digital data from ADCs,
- *   - develop algorithms, or build digital systems to process signals/data in real time
+ * This project is designed for the course Embedded Signal Processing Systems.
+ * The program provides a software framework that has implemented 2xADC, 2xDAC,  
+ * an external trigger and an USART. Therefore, one can 
+ *   - access the digital data from ADCs,  
+ *   - develop algorithms, or build digital systems to process signals/data in real time 
  *   - send processed signals/data out to DACs and get analog signals
  *   - send data from the MCU to PCs via USART
- *
- * The configurations for the above functions are
+ * 
+ * The configurations for the above functions are the following
  *   - J2's ADC1 and ADC5 are configured as two ADC channels: ADC0 and ADC1
  *   - J4's SCK and J2's ADC2 are defined as two DAC channels: DAC0 and DAC1
- *   - J2's ADC4 is configured as external trigger
+ *   - J4's SCL is configured as external trigger
  *   - J4's TX is for USART
+ * 
+ * When this project is running, the output can be obtained ONLY when the external 
+ * trigger is activated (which can be made just by touching your finger on J4's SCL).
+ * A controller (using "counter") is available to control the time instant 
+ * when the next external trigger is enabled
+ *
+ * When programming for DSP applications, the starting point is "DO DSP HERE" in main(void).
+ * You get to it via Ctrl+F (Find) 
  *
  * Copyright (c) 2014 Ping Wu. All rights reserved.
  ********************************************************************************/
@@ -26,20 +35,19 @@
 #define U_OSC0_START        3               // OSC0 start up time
 #define PDCA_CHANNEL_ADC    0               // DMA Channel 0 is for ADC
 #define PDCA_CHANNEL_DAC    1               // DAC
-#define PDCA_CHANNEL_USART  2               // Usart
-#define Buffer_Size         4096            // ADC Buffer Size which must be 4^n times 256 ?
-#define N_of_bits			10				
-#define DATA_SIZE			1024			// 2^N_of_bits
-#define TC                  (&AVR32_TC0)    // Timer/Counter
-#define TC_CHANNEL          0                // ??
-#define PROCESSING          0                // If DSP_STATE == PROCESSING, the board is doing DSP
-#define LOADING             1                // If DSP_STATE == LAODING, the board is loading data from ADC
-#define STANDSTILL          2                // If DSP_STATE == STANDSTILL, the board is standing still
-
-#define Ext_Trigger_Pin  AVR32_ADC_AD_4_PIN      // Pin J2's ADC4 is defined as Ext trigger
+#define Buffer_Size         4096            // DSP Buffer Size which must be a number that is 4^n 
+#define DATA_SIZE			1024			// DSP FFT DATA SIZE = 2^N_of_bits
+#define N_of_bits			10				// FFT orders
+#define TC                  (&AVR32_TC0)	// Timer/Counter
+#define TC_CHANNEL          0				// Timer Channel 0
+#define PROCESSING          0				// If DSP_STATE == PROCESSING, the board is doing DSP 
+#define LOADING				1				// If DSP_STATE == LAODING, the board is loading data from ADC
+#define STANDSTILL          2				// If DSP_STATE == STANDSTILL, the board is standing still
+#define ADC_FREQUENCY       FOSC0/256
+#define Ext_Trigger_Pin  AVR32_PIN_PA15		// Pin J4's SCL is defined as Ext trigger
 
 static volatile uint32_t trigger=0;
-static volatile DSP_STATE = LOADING;
+static volatile uint16_t DSP_STATE = LOADING;
 static void User_Usart_int2str(uint32_t User_int32);
 static avr32_tc_t *tc=TC;
 /************************************************************************/
@@ -48,16 +56,23 @@ static avr32_tc_t *tc=TC;
 static volatile uint16_t ADC_PDCA_BUFFER1[Buffer_Size*2];
 static volatile uint16_t DAC_PDCA_BUFFER1[Buffer_Size*2];
 //DSP BUFFERS
-static volatile dsp16_t  DSP_REAL_BUFFER[Buffer_Size];
+static volatile uint16_t  DSP_REAL_BUFFER[Buffer_Size];
 static volatile dsp16_complex_t DSP_COMP_BUFFER[Buffer_Size];
 
 /************************************************************************/
 
 //Declare pointers
 static uint16_t *DAC_BUFFER_PTR   =&DAC_PDCA_BUFFER1;
-static  dsp16_t  *DSP_BUFFER_PTR   =&DSP_REAL_BUFFER;
+static uint16_t *DSP_BUFFER_PTR   =&DSP_REAL_BUFFER;
 //Timer Tick
 static volatile uint32_t tc_tick=0;
+static volatile uint32_t counter = 0 ;
+/************************************************************************/
+/*                            New Things For This                       */
+/************************************************************************/
+
+
+/************************************************************************/
 
 /************************************************************************/
 /*                             PDCA Options                             */
@@ -86,8 +101,10 @@ static const pdca_channel_options_t DAC_PDCA_OPT = {
     .r_size = NULL,
     .transfer_size = PDCA_TRANSFER_SIZE_WORD,
 };
+
+
 /************************************************************************/
-/* INTERRUPTS                              */
+/*                              INTERRUPTS                              */
 /************************************************************************/
 
 /*----------------------------------------------------------------------*/
@@ -118,45 +135,58 @@ ISR(tc_irq,AVR32_TC0_IRQ0,AVR32_INTC_INT3)
 {
     tc_read_sr(TC, TC_CHANNEL);
     //convert 10-bit ADC data to 16 bits & then transfer to DAC & DSP buffers
-    DAC_BUFFER_PTR[2*tc_tick]  = ADC_PDCA_BUFFER1[2*tc_tick+1]<<6^0x8000;
-    DAC_BUFFER_PTR[2*tc_tick+1]= ADC_PDCA_BUFFER1[2*tc_tick+1]<<6^0x8000;
+    DAC_BUFFER_PTR[2*tc_tick]  = ADC_PDCA_BUFFER1[2*tc_tick+1]<<6^0x8000; //ADC CH1 to DAC CH1
+    DAC_BUFFER_PTR[2*tc_tick+1]= ADC_PDCA_BUFFER1[2*tc_tick+1]<<6^0x8000; //ADC CH5 to DAC CH0
     tc_tick++;
     if (tc_tick >= Buffer_Size) tc_tick =0;
 }
-
-ISR(User_Ext_Trigger_Isr,AVR32_GPIO_IRQ_0,AVR32_INTC_INT0)
+/*----------------------------------------------------------------------*/
+/*   EXTERNAL TRIGGER INTERRUPT
+     Use for One Shot DSP 
+     Reset DMAs and set DSP state to LOADING and timer tick
+*/
+/*----------------------------------------------------------------------*/
+ISR(User_Ext_Trigger_Isr,AVR32_GPIO_IRQ_1,AVR32_INTC_INT0)
 {
-    gpio_toggle_pin(LED1_GPIO);
-    DSP_STATE = LOADING;
-    pdca_enable(PDCA_CHANNEL_ADC);
-    pdca_enable(PDCA_CHANNEL_DAC);
-    tc_tick=0;
-    gpio_clear_pin_interrupt_flag(Ext_Trigger_Pin);
+	gpio_toggle_pin(LED1_GPIO);
+	DSP_STATE = LOADING;
+	pdca_enable(PDCA_CHANNEL_ADC);	//reset DMA
+	pdca_enable(PDCA_CHANNEL_DAC);	//reset DMA
+	tc_tick=0;						//reset index
+	counter =0;						//reset counter
+	gpio_clear_pin_interrupt_flag(Ext_Trigger_Pin);
+	gpio_disable_pin_interrupt(Ext_Trigger_Pin);
 }
 
+/*----------------------------------------------------------------------*/
+/*   ADC TO DSP TO DAC INTERRUPT
+        Both Continuously and One shot 
+*/
+/*---------------------------------------------------------------------*/
 ISR(tc_irq_dsp,AVR32_TC0_IRQ0,AVR32_INTC_INT3)
 {
-    tc_read_sr(TC, TC_CHANNEL);
-    //put the ADC data to DAC and DSP buffers
-    switch (DSP_STATE)
-    {
-        case LOADING :
-DSP_BUFFER_PTR[tc_tick]=((ADC_PDCA_BUFFER1[2*tc_tick])<<6)/2;
-            break;
-        case PROCESSING:
-            gpio_toggle_pin(LED0_GPIO);
-            break;
-        case STANDSTILL:
-            gpio_toggle_pin(LED3_GPIO);
-        break;
-    }
-    tc_tick++;
-    if (tc_tick >= Buffer_Size)
-    {
-        tc_tick =0;
-        if (DSP_STATE==LOADING) DSP_STATE = PROCESSING;
-        gpio_toggle_pin(LED2_GPIO);
-    }
+	tc_read_sr(TC, TC_CHANNEL);
+	//put the ADC data to DAC and DSP buffers
+	switch (DSP_STATE)
+	{
+		case LOADING :		// load the data of "Buffer_Size" to the DSP buffer
+			DSP_BUFFER_PTR[tc_tick]=(ADC_PDCA_BUFFER1[2*tc_tick]<<6)/2;
+			break;
+		case PROCESSING:
+			gpio_toggle_pin(LED0_GPIO);
+			break;
+		case STANDSTILL:
+			gpio_toggle_pin(LED3_GPIO);
+		break;
+	}
+	tc_tick++;
+	counter++;
+	if (tc_tick >= Buffer_Size)
+	{	
+		tc_tick =0;
+		if ( DSP_STATE == LOADING)  DSP_STATE = PROCESSING;	
+		gpio_toggle_pin(LED2_GPIO);
+	}
 }
 /************************INTERRUPTS**************************************/
 
@@ -261,18 +291,17 @@ static void tc_init(volatile avr32_tc_t *tc,uint32_t ADC_FREQ)
 /*----------------------------------------------------------------------*/
 static void pdca_set_irq(void)
 {
-    #if __GNUC__
-        INTC_init_interrupts();
+	#if __GNUC__
+	    INTC_init_interrupts();
 
     //All PDCA interrupts need to be registered here
-        INTC_register_interrupt(&pdca_adc_handler, AVR32_PDCA_IRQ_0,
-                AVR32_INTC_INT0);
-        INTC_register_interrupt(&pdca_dac_handler, AVR32_PDCA_IRQ_1,
-                AVR32_INTC_INT0);
-INTC_register_interrupt(&User_Ext_Trigger_Isr,AVR32_GPIO_IRQ_2,AVR32_INTC_INT3); //*3
-INTC_register_interrupt(&tc_irq_dsp,AVR32_TC0_IRQ0,AVR32_INTC_INT2);//*2 *3
-//INTC_register_interrupt(&tc_irq,AVR32_TC0_IRQ0,AVR32_INTC_INT2); //*1
-    #endif
+	    INTC_register_interrupt(&pdca_adc_handler, AVR32_PDCA_IRQ_0,
+		        AVR32_INTC_INT0);
+		INTC_register_interrupt(&pdca_dac_handler, AVR32_PDCA_IRQ_1,
+			    AVR32_INTC_INT0);
+		INTC_register_interrupt(&User_Ext_Trigger_Isr,AVR32_GPIO_IRQ_1,AVR32_INTC_INT3);
+		INTC_register_interrupt(&tc_irq_dsp,AVR32_TC0_IRQ0,AVR32_INTC_INT2);
+	#endif
 
     /* Enable all interrupt/exception. */
     Enable_global_interrupt();
@@ -281,113 +310,120 @@ INTC_register_interrupt(&tc_irq_dsp,AVR32_TC0_IRQ0,AVR32_INTC_INT2);//*2 *3
 /************************* End of SUB FUNCTIONS ***************************/
 
 
-
-
 /************************************************************************/
-/* MAIN                                   */
+/*                               MAIN                                   */
 /************************************************************************/
 int main (void)
 {
-	board_init();
-	sysclk_init();
-	//The pins and functions to be used
-	const gpio_map_t usart_gpio_map = {
-		{AVR32_ABDAC_DATA_0_0_PIN,AVR32_ABDAC_DATA_0_0_FUNCTION}, // Configure pin J4's SCK as DAC0
-		{AVR32_ABDAC_DATA_1_1_PIN,AVR32_ABDAC_DATA_1_1_FUNCTION}, // Configure pin J2's ADC2 as DAC1
-		{AVR32_ADC_AD_1_PIN,AVR32_ADC_AD_1_FUNCTION}, // Configure pin J2's ADC1 as ADC
-		{AVR32_ADC_AD_5_PIN,AVR32_ADC_AD_5_FUNCTION}, // Configure pin J2's ADC5 as ADC
-		{USART_TXD_PIN,USART_TXD_FUNCTION},
-		{USART_RXD_PIN,USART_RXD_FUNCTION},
-	};
+    board_init();
+    sysclk_init();
+    //The pins and functions to be used
+    const gpio_map_t usart_gpio_map = {
+        {AVR32_ABDAC_DATA_0_0_PIN,AVR32_ABDAC_DATA_0_0_FUNCTION},   // Configure pin J4's SCK as DAC0
+        {AVR32_ABDAC_DATA_1_1_PIN,AVR32_ABDAC_DATA_1_1_FUNCTION},   // Configure pin J2's ADC2 as DAC1
+        {AVR32_ADC_AD_1_PIN,AVR32_ADC_AD_1_FUNCTION},				// Configure pin J2's ADC1 as ADC
+        {AVR32_ADC_AD_5_PIN,AVR32_ADC_AD_5_FUNCTION},				// Configure pin J2's ADC5 as ADC
+        {USART_TXD_PIN,USART_TXD_FUNCTION},
+        {USART_RXD_PIN,USART_RXD_FUNCTION},
+        {65,2},
+    };
 
-	//Usart options
-	//no parity, no flow control, one stop bit
-	static usart_options_t User_Usart_OPT = {
-		.baudrate    = 57600,                    //due to LM358's speed, Baudrate can't fast than 57600.
-		.channelmode = USART_NORMAL_CHMODE,
-		.charlength  = 8,
-		.paritytype  = USART_NO_PARITY,
-		.stopbits    = USART_1_STOPBIT,
-	};
+    //Usart options
+    //no parity, no flow control, one stop bit
+    static usart_options_t User_Usart_OPT = {
+        .baudrate    = 57600,                    //due to LM358's speed, Baudrate can't fast than 57600.
+        .channelmode = USART_NORMAL_CHMODE,
+        .charlength  = 8,
+        .paritytype  = USART_NO_PARITY,
+        .stopbits    = USART_1_STOPBIT,
+    };
 
-	//Clock options
-	static pm_freq_param_t clockSettings;
-	clockSettings.cpu_f = U_CPU_F;
-	clockSettings.osc0_f = U_OSC0_F;
-	clockSettings.osc0_startup = U_OSC0_START;
-	clockSettings.pba_f = U_PBA_F;
+    //Clock options
+    static pm_freq_param_t clockSettings;
+        clockSettings.cpu_f = U_CPU_F;
+        clockSettings.osc0_f = U_OSC0_F;
+        clockSettings.osc0_startup = U_OSC0_START;
+        clockSettings.pba_f = U_PBA_F;
 
-	//set clock with the options
-	if (pm_configure_clocks(&clockSettings) != PM_FREQ_STATUS_OK)
-	{
-		while(1){};
-	}
-	uint32_t ADC_FREQ = FOSC0/256;
+    //set clock with the options
+    if (pm_configure_clocks(&clockSettings) != PM_FREQ_STATUS_OK)
+    {
+        while(1){};
+    }
+	/*  Set ADC Sampling Frequency */ 
+    uint32_t ADC_FREQ = ADC_FREQUENCY;
 
-	//enable pins
-	gpio_enable_module(usart_gpio_map,
-	sizeof(usart_gpio_map) / sizeof(usart_gpio_map[0]));
+    //enable pins
+    gpio_enable_module(usart_gpio_map,
+        sizeof(usart_gpio_map) / sizeof(usart_gpio_map[0]));
 
-	//ADC prescal setting
-	//ADC clock = clock / ((prescal+1)*2)
-	AVR32_ADC.mr |= 0x01 << AVR32_ADC_MR_PRESCAL_OFFSET;
-	/*ADC Trigger setting. Trigger is Timer 0 channel 0 RA */
-	AVR32_ADC.mr |= 0x00 << AVR32_ADC_MR_TRGSEL_OFFSET;
-	AVR32_ADC.mr |= 0x01 << AVR32_ADC_MR_TRGEN_OFFSET;
-	adc_configure(&AVR32_ADC);
-	adc_enable(&AVR32_ADC,AVR32_ADC_CH1);        // enable ADC1
-	adc_enable(&AVR32_ADC,AVR32_ADC_CH5);        // enable ADC5
+    //ADC prescal setting
+    //ADC clock = clock / ((prescal+1)*2)
+    AVR32_ADC.mr |= 0x01 << AVR32_ADC_MR_PRESCAL_OFFSET;
+    /*ADC Trigger setting. Trigger is Timer 0 channel 0 RA */
+    AVR32_ADC.mr |= 0x00 << AVR32_ADC_MR_TRGSEL_OFFSET;
+    AVR32_ADC.mr |= 0x01 << AVR32_ADC_MR_TRGEN_OFFSET;
+    adc_configure(&AVR32_ADC);
+    adc_enable(&AVR32_ADC,AVR32_ADC_CH1);        // enable ADC1
+    adc_enable(&AVR32_ADC,AVR32_ADC_CH5);        // enable ADC5
 
-	//DAC init, sampling frequency = FOSC0/256;
-	abdac_set_dac_hz(&AVR32_ABDAC,FOSC0,FOSC0/256);
-	abdac_enable(&AVR32_ABDAC);
+    //DAC init, sampling frequency = FOSC0/256;
+    abdac_set_dac_hz(&AVR32_ABDAC,FOSC0,FOSC0/256);
+    abdac_enable(&AVR32_ABDAC);
 
-	// External trigger
+	// External trigger 
 	gpio_enable_gpio_pin(Ext_Trigger_Pin);
 	gpio_configure_pin(Ext_Trigger_Pin,GPIO_DIR_INPUT);
 	gpio_enable_pin_interrupt(Ext_Trigger_Pin,GPIO_RISING_EDGE);
-	//Usart init wait until success
-	while(usart_init_rs232(USART,&User_Usart_OPT,U_PBA_F) != USART_SUCCESS);
-	//test if communication is ok
-	usart_write_line(USART,"USART OK!! \r\n");
+    //Usart init wait until success
+    while(usart_init_rs232(USART,&User_Usart_OPT,U_PBA_F) != USART_SUCCESS);
+    //test if communication is ok
+    usart_write_line(USART,"USART OK!! \r\n");
 
-	// Initialize all PDCA channels to be used
-	pdca_init_channel(PDCA_CHANNEL_ADC,&ADC_PDCA_OPT);
-	pdca_init_channel(PDCA_CHANNEL_DAC,&DAC_PDCA_OPT);
+    // Initialize all PDCA channels to be used 
+    pdca_init_channel(PDCA_CHANNEL_ADC,&ADC_PDCA_OPT);
+    pdca_init_channel(PDCA_CHANNEL_DAC,&DAC_PDCA_OPT);
 
-	//All PDCA channels need to enable corresponding interrupts
-	pdca_enable_interrupt_transfer_complete(PDCA_CHANNEL_ADC);
-	pdca_enable_interrupt_reload_counter_zero(PDCA_CHANNEL_DAC);
+    //All PDCA channels need to enable corresponding interrupts
+    pdca_enable_interrupt_transfer_complete(PDCA_CHANNEL_ADC);
+    pdca_enable_interrupt_reload_counter_zero(PDCA_CHANNEL_DAC);
 
-	//call interrupt settings
-	pdca_set_irq();
 
-	//Enable all channels before the data begin to be read or written
-	pdca_enable(PDCA_CHANNEL_ADC);
-	pdca_enable(PDCA_CHANNEL_DAC);
-	//timer init
-	tc_init(tc,ADC_FREQ);
-	while (1)
-	{
+    //call interrupt settings
+    pdca_set_irq();
+
+    //Enable all channels before the data begin to be read or written 
+    pdca_enable(PDCA_CHANNEL_ADC);
+    pdca_enable(PDCA_CHANNEL_DAC);
+    //timer init
+    tc_init(tc,ADC_FREQ);
+    while (1)
+    {	
+		if (counter >= 178*(ADC_FREQ/1000)) // control the time when the next Ext_Trigger is valid
+		{						 // counter = the number of cycles; "counter = ADC_FREQ" corresponds to the time of 1 second
+			gpio_enable_pin_interrupt(Ext_Trigger_Pin,GPIO_RISING_EDGE);  // "counter = ADC_FREQ/1000" corresponds to 1 ms
+		}
 		if (DSP_STATE==PROCESSING)
 		{
-			/*......................................................................*/
-			/*            Do DSP HERE                                               */
-			/*......................................................................*/
-			usart_write_line(USART,"MATLAB");
-			for (int ind=0;ind<= Buffer_Size;ind++)
+			for (int ind=0;ind< Buffer_Size;ind++)	
 			{
-				DAC_BUFFER_PTR[2*ind] = (DSP_REAL_BUFFER[ind])*2^0x8000;
-				DAC_BUFFER_PTR[2*ind+1]=(DSP_REAL_BUFFER[ind])*2^0x8000;
+				DAC_BUFFER_PTR[2*ind] = (DSP_REAL_BUFFER[ind]*2^0x8000);	
+				DAC_BUFFER_PTR[2*ind+1]=(DSP_REAL_BUFFER[ind]*2^0x8000);	
 			}
-			for (int ind=0;ind<= DATA_SIZE;ind++)
-			{
+/*......................................................................*/
+/*            Do DSP HERE                                               */
+/*......................................................................*/
+			for (int ind=0;ind<= DATA_SIZE;ind++){
+
 				User_Usart_int2str(DSP_REAL_BUFFER[ind]*2);
 				usart_write_line(USART,",");
 			}
-			/*......................................................................*/
-			DSP_STATE=STANDSTILL;
+
+
+
+/*......................................................................*/
+			DSP_STATE=STANDSTILL;  // 
 		}
-	}
+    }
 }
 /***************************** End of MAIN *************************************/
